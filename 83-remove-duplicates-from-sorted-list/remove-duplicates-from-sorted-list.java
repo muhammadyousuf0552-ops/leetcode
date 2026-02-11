d6b7651adb5f3e93136e88537d408e09688f7492
muhammadyousuf0552-ops <muhammadyousuf0552@gmail.com>
2026-02-11 19:52:09 +0500

    Time: 0 ms (100.00%) | Memory: 45.5 MB (67.68%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;   // remove duplicate
+            } else {
+                curr = curr.next;             // move forward
+            }
+        }
+
+        return head;
+    }
+}
